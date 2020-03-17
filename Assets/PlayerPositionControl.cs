566ce707b3a40e120fcc73762c3607af97a724a7
Bryan <kirbyderby2000@gmail.com>
2020-03-16 17:36:33 -0700

    Changed the import settings of all the environment 3D models. "Read/Write Enabled" had to be set ON in the import settings. Not sure why the Unity Editor didn't complain about this but this issue is debugged like crazy within a development build of the project. Also added a player position controller script that tries to reset the player's position when they fall off the map.

---
+++ Assets/PlayerPositionControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPositionControl : MonoBehaviour
+{
+    [SerializeField] float minYPosition;
+    [SerializeField] CharacterController charController;
+
+    Vector3 lastGoodPosition;
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+        StartCoroutine(ControlPalyerPosition());
+    }
+
+    IEnumerator ControlPalyerPosition()
+    {
+        lastGoodPosition = transform.position;
+        while (true)
+        {
+            yield return new WaitForSeconds(3.0f);
+
+            if(transform.position.y < minYPosition)
+            {
+                charController.enabled = false;
+                transform.position = lastGoodPosition;
+                charController.enabled = true;
+            }
+            else
+            {
+                lastGoodPosition = transform.position;
+            }
+        }
+    }
+}
